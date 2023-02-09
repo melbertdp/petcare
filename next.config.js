a25dd503ec17602470e39af2dd1313faf3b95db2
melbertdp <melbert.delapaz@gmail.com>
2023-02-10 03:40:49 +0800

    pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  // disable: process.env.NODE_ENV === 'development',
+  
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+})
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withPWA({
+  // next.js config
+})
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html className="h-full bg-gray-50 antialiased " lang="en">
       <Head>
+        <link rel='manifest' href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,600;0,800;1,600;1,800&display=swap" rel="stylesheet" />
