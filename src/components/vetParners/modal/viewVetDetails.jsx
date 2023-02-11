1bbef30e314ac12592f66520abae222e0bf4bdb1
melbertdp <melbert.delapaz@gmail.com>
2023-02-12 03:36:57 +0800

    minor fix

--- src/components/vetParners/modal/viewVetDetails.jsx
+++ src/components/vetParners/modal/viewVetDetails.jsx
@@ -26,7 +26,7 @@ export default function FindVet({ vetDetails, open, setOpen }) {
                 </Transition.Child>
 
                 <div className="fixed inset-0 z-[99] overflow-y-auto">
-                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
                         <Transition.Child
                             as={Fragment}
                             enter="ease-out duration-300"
@@ -63,7 +63,7 @@ export default function FindVet({ vetDetails, open, setOpen }) {
                                                             </div>
                                                             <div class="flex">
                                                                 <span class="title-font font-medium text-2xl text-gray-900">$58.00</span>
-                                                                <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Book Consultation</button>
+                                                                <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Book</button>
                                                                 <button title="add to favorites" class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                                                                     <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                                                                         <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
